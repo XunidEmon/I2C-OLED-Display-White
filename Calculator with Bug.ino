8d7b91a44d1ea193e49fd0e32713af2641565ac0
Emon <mdxunidemon@icloud.com>
2026-05-04 02:17:54 +0600

    Add basic calculator functionality with OLED display

---
+++ Calculator with Bug.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 oled(128, 64, &Wire, -1);
+
+int num1, num2;
+
+void setup() {
+  Wire.begin();
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  oled.clearDisplay();
+  oled.setTextSize(2);
+  oled.setTextColor(SSD1306_WHITE);
+  
+  Serial.begin(9600);
+  Serial.println("Enter two numbers:");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    num1 = Serial.parseInt();
+    num2 = Serial.parseInt();
+    
+    int sum = num1 + num2;
+    
+    oled.clearDisplay();
+    oled.setCursor(0, 0); 
+    oled.print("Num1: "); 
+    oled.print(num1);
+    oled.setCursor(0, 20); 
+    oled.print("Num2: "); 
+    oled.print(num2);
+    oled.setCursor(0, 40); 
+    oled.print("Sum: ");  
+    oled.print(sum);
+    oled.display();
+    
+    Serial.print("Sum: "); 
+    Serial.println(sum);
+    Serial.println("Enter two numbers:");
+  }
+}
